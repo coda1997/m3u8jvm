c944b881aa06fb16bfa92a5d39eb21c844673b7b
DadaChen1997 <overlooker1997@gmail.com>
2020-10-04 10:05:29 +0800

    test case

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,21 @@
 fun main(args: Array<String>) {
-    m3u8download("sample.m3u8","sample file path", 25)
+    if(args.isEmpty()){
+       error("please enter the url.")
+        return
+    }
+    val url = args[0].substring(1 until args[0].length-1)
+    var outputPath = "."
+    var c = 25
+    if(args.size>1){
+        for (i in 1 until args.size-1){
+            if(args[i].startsWith( "-o")){
+                outputPath = args[i].substring(args[i].indexOf(':')+1)
+            }
+            if(args[i].startsWith( "-c")){
+                c = args[i].substring(args[i].indexOf(':')+1).toInt()
+            }
+        }
+    }
+    println("config info: url = $url, output path = $outputPath, number = $c")
+    m3u8download(url, outputPath, c)
 }
